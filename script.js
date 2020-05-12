53f22b66c421d7cfe67d7a4758925a30d95be601
Glitch (hello-webpage) <none>
2020-05-12 12:50:42 +0000

    💝🍕 Checkpoint ./script.js:574715/1338

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 // Name any p5.js functions we use in the global so Glitch can recognize them.
-/* global createCanvas, colorMode, HSB, width, height, 
-          random, background, fill, color, random
-          rect, ellipse, stroke, image, loadImage, 
-          collideCircleCircle, text, mouseX, mouseY, 
-          strokeWeight, line, mouseIsPressed, windowWidth, 
-          windowHeight, noStroke */
+// Add to this list as you consult the p5.js documentation for other functions. 
+/* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
+          rect, ellipse, stroke, image, loadImage, collideCircleCircle, text, mouseX, mouseY, 
+          strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke */
 
-let backgroundColor
+let backgroundColor;
 
 function setup() {
   // Canvas & color settings
-  createCanvas(400, 400)
-  colorMode(HSB, 360, 100, 100)
-  backgroundColor = 95
+  createCanvas(400, 400);
+  colorMode(HSB, 360, 100, 100);
+  backgroundColor = 95;
 }
 
 function draw() {
-  background(backgroundColor)
+  background(backgroundColor);
 }
