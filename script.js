3326f3ec7136b98303a66a8acc2143c93413db21
Glitch (hello-webpage) <none>
2020-07-27 17:49:00 +0000

    🍱🍅 Checkpoint ./script.js:18045350/531:18574881/377

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
 /* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
           rect, ellipse, stroke, image, loadImage, collideCircleCircle, text, mouseX, mouseY, 
           strokeWeight, collideRectCircle, keyCode, mouseX, mouseY, line, mouseIsPressed, 
-          windowWidth, windowHeight, noStroke, LEFT_ARROW, RIGHT_ARROW */  
+          windowWidth, windowHeight, noStroke, LEFT_ARROW, RIGHT_ARROW keyIsDown*/  
 
-let playerImage, player1
+let playerImage, player1, vel;
+let platforms = [];
 
 function setup() {
   // Canvas & color settings
@@ -13,20 +14,21 @@ function setup() {
   colorMode(HSB, 360, 100, 100);
   playerImage = loadImage("https://cdn.glitch.com/bb52dd36-2050-4746-bc69-96e74a13122e%2Fdoodlejumpthing.png?v=1595870028038")
   player1 = new Player()
+  vel = 2
   
 }
 
 function draw() {
   background(230,70,80);
   player1.draw()
-  player1.update();
+ // player1.update();
 }
 
 
 class Player{
   constructor() {
     this.x = 200;
-    this.y = 500;
+    this.y = 200;
     this.size = 40;
     this.gravity = 0.9
     this.lift = -16
@@ -44,21 +46,35 @@ class Player{
     this.y += this.velocity
     
     if(this.y >= height){
-      this.x = 0;
-      this.y = 0;
-      this.y = height - 101
-    this.velocity = 0;
+      this.y = height - 100
+      this.velocity = 0;
   }
   
   }
   }
 
-function Platform(){
+class Platform {
+  constructor() {
+    this.x = random(width);
+    this.y = random(height);
+    this.width = 20;  
+    this.height = 10;
+    this.speed = 5;
+}
   
+  show()
+  {
+    fill(255);
+    rect(this.x, this.y, this.x + this.width, this.y + this.height);
+  }
   
+  update()
+  {
+    this.y += this.speed;
+  }
 }
 
-function keyPressed()
+/*function keyPressed()
 {
   if (keyCode === LEFT_ARROW)
     {
@@ -67,5 +83,15 @@ function keyPressed()
   else if (keyCode === RIGHT_ARROW)
     {
       player1.x += 20;
-    }
+    } 
+}*/
+
+function keyDown() {
+  
+  if (keyIsDown(LEFT_ARROW)) {
+    player1.x -= vel;
+  }
+  else if (keyIsDown(RIGHT_ARROW)) {
+    player1.x += vel;
+  }
 }
