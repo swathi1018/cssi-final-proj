95bbe171652a0a07cb014e82251afef40e52eb5b
Glitch (hello-webpage) <none>
2020-07-27 17:25:17 +0000

    📅🐴 Checkpoint ./script.js:18045350/865:18574881/646

--- script.js
+++ script.js
@@ -2,26 +2,37 @@
 // Add to this list as you consult the p5.js documentation for other functions.
 /* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
           rect, ellipse, stroke, image, loadImage, collideCircleCircle, text, mouseX, mouseY, 
-          strokeWeight, collideRectCircle, keyCode, line, mouseIsPressed, windowWidth, windowHeight, noStroke */  
+          strokeWeight, collideRectCircle, keyCode, mouseX, mouseY, line, mouseIsPressed, windowWidth, windowHeight, noStroke */  
+
+let playerImage
 
 function setup() {
   // Canvas & color settings
   createCanvas(400, 600);
   colorMode(HSB, 360, 100, 100);
+  playerImage = loadImage("https://cdn.glitch.com/bb52dd36-2050-4746-bc69-96e74a13122e%2Fdoodlejumpthing.png?v=1595870028038")
+  
   
 }
 
 function draw() {
-  background(236,90,25);
-  
+  background(230,70,80);
+  image(playerImage, 200, 200, 40, 40);
 }
 
 
-function Player(){
+class Player{
+  
+  
   
 }
 
 function Platform(){
   
+  
+}
+
+function keyPressed()
+{
   
 }
