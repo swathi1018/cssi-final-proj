3b00133ad5bf8b7bb6e08254b86d7333fe43a4a2
Glitch (hello-webpage) <none>
2020-07-27 18:12:23 +0000

    👶🍏 Checkpoint ./script.js:18045350/298:18574881/403

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 /* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
           rect, ellipse, stroke, image, loadImage, collideCircleCircle, text, mouseX, mouseY, 
           strokeWeight, collideRectCircle, keyCode, mouseX, mouseY, line, mouseIsPressed, 
-          windowWidth, windowHeight, noStroke, LEFT_ARROW, RIGHT_ARROW keyIsDown*/  
+          windowWidth, windowHeight, noStroke, LEFT_ARROW, RIGHT_ARROW, frameCount, keyIsDown*/  
 
-let playerImage, player1, vel;
+let playerImage, player1, vel, score;
 let platforms = [];
 
-const plat
+const numPlatforms = 10;
 
 function setup() {
   // Canvas & color settings
@@ -17,20 +17,30 @@ function setup() {
   playerImage = loadImage("https://cdn.glitch.com/bb52dd36-2050-4746-bc69-96e74a13122e%2Fdoodlejumpthing.png?v=1595870028038")
   player1 = new Player()
   vel = 2
+  score = 0
   
-  platforms.push(new Platform());
-  
+  for (let i = 0; i < numPlatforms; i++)
+    {
+      platforms.push(new Platform());
+    }  
 }
 
 function draw() {
   keyDown();
   background(230,70,80);
   player1.draw()
- // player1.update();
+  player1.update();
   
+  if (frameCount % 60 == 0)
+    {
+      platforms.push(new Platform());
+      platforms.push(new Platform());
+      platforms.push(new Platform());
+    }
   for (let i = 0; i < platforms.length; i++)
     {
       platforms[i].show();
+      platforms[i].update();
     }
 }
 
@@ -51,14 +61,23 @@ class Player{
   }
   
   update(){
-    this.velocity += this.gravity
+   /* this.velocity += this.gravity
     this.velocity *= 0.9
     this.y += this.velocity
     
     if(this.y >= height){
       this.y = height - 100
       this.velocity = 0;
-  }
+     } */
+    
+    if(this.x < 0){
+      this.x = width
+    }
+    
+    if(this.x > width){
+      this.x = 0
+    }
+    
   
   }
   }
@@ -66,15 +85,21 @@ class Player{
 class Platform {
   constructor() {
     this.x = random(width);
-    this.y = random(height);
+    this.y = random(height/4);
     this.width = 50;  
     this.height = 5;
+    this.speed = 2;
 }
   
   show()
   {
     fill(255);
     rect(this.x, this.y, this.width, this.height);
+  }
+  
+  update()
+  {
+    this.y += this.speed
   }
   
 }
